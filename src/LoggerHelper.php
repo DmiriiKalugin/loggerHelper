8c0d3a9fcf87433f1cb33354d4c472dc383cf03e
Дмитрий Калугин <d.kalugin@sitisit.ru>
2024-09-22 17:06:00 +0500

    first commit

---
+++ src/LoggerHelper.php
@@ -0,0 +1,26 @@
+<?php
+
+class LoggerHelper
+{
+    private const GREEN = "\033[32m";
+    private const RED = "\033[31m";
+    private const RESET = "\033[0m";
+
+    /**
+     * @param string $message
+     * @return void
+     */
+    public static function error(string $message): void
+    {
+        echo self::RED . 'Error: ' . date('Y-m-d H:i:s') . ' - ' . $message . self::RESET . PHP_EOL;
+    }
+
+    /**
+     * @param string $message
+     * @return void
+     */
+    public static function debug(string $message): void
+    {
+        echo self::GREEN . 'DEBUG: ' . date('Y-m-d H:i:s') . ' - ' . $message . self::RESET . PHP_EOL;
+    }
+}
